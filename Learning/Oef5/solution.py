173d2efaaf4e2386e56b199191e0315ff09ad53a
HendrikVE <18201166+Hennots@users.noreply.github.com>
2021-03-25 23:59:41 +0100

    new exec

---
+++ Learning/Oef5/solution.py
@@ -0,0 +1,13 @@
+def firstLetterOfString(s):
+    print(s)
+
+
+Wrong = "Wrong bye bye"
+assert firstLetterOfString("hello") == 'h', Wrong
+assert firstLetterOfString("myname") == 'm', Wrong
+assert firstLetterOfString("xylofoon") == 'x', Wrong
+assert firstLetterOfString("snoep") == 's', Wrong
+assert firstLetterOfString("ijs") == 'i', Wrong
+assert firstLetterOfString("nok") == 'n', Wrong
+
+print('Great succes is very nice')
